3414531574807e4f7cecbaf45de4a085bb190eff
Pedro Augusto Maia Silva <pedroaugustoms14@gmai.com>
2019-11-23 18:28:09 -0300

    alteração da interface

--- src/libinterface/interface.c
+++ src/libinterface/interface.c
@@ -1,10 +1,123 @@
 #include "word.h"
 void Interface(){
-    int escolha1,escolha2;
-    printf("Escolha uma das opções de TAD's abaixo:\n");
-    printf("1-TAD-palavra\n");printf("2-TAD-texto\n");printf("3-TAD-biblioteca");
-    scanf("%d",&escolha1);
-    printf("Escolha qual das implementacoes deseja usar:\n");
-    printf("1-Vetor\n");printf("2-Dinamica");
-    scanf("%")
+    int escolha1,escolha2,escolha3,escolha4;
+    printf("Deseja que todas as implementacoes sejam usadas ?");
+    printf("1-sim\n");printf("2-nao");
+    scanf("%d",&escolha3);
+    if(escolha3==1){
+        //utiliza todos os cenarios de teste
+    }
+    else{
+        printf("Escolha uma das opções de TAD's abaixo:\n");
+        printf("1-TAD-palavra\n");printf("2-TAD-texto\n");printf("3-TAD-biblioteca");
+        scanf("%d",&escolha1);
+        printf("Escolha qual das implementacoes deseja usar:\n");
+        printf("1-Vetor\n");printf("2-Dinamica");
+        scanf("%d",escolha2);
+        printf("Qual das opcoes abaixo de ordenacao deseja utilizar:\n");
+        printf("1-Bobblesort\n");printf("2-Insertsort\n");printf("3-Selectsort\n");printf("4-Quicksort\n");printf("5-Shellsort");
+        scanf("%d",escolha4);
+        if(escolha1==1 && escolha2==1){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+        else if(escolha1==2 && escolha2==1){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+        else if(escolha1==3 && escolha2==1){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+        else if(escolha1==1 && escolha2==2){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+        else if(escolha1==2 && escolha2==2){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+        else if(escolha1==3 && escolha2==2){
+            if(escolha4==1){
+
+            }
+            else if(escolha4==2){
+
+            }
+            else if(escolha4==3){
+
+            }
+            else if(escolha4==4){
+
+            }
+            else if(escolha4==5){
+
+            }
+        }
+    }
 }
