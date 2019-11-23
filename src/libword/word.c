d478731ea5dbd232f1f97dc7957845e9bfa6d5b8
Pedro Augusto Maia Silva <pedroaugustoms14@gmai.com>
2019-11-23 16:59:32 -0300

    Hello!!

--- src/libword/word.c
+++
@@ -1,9 +0,0 @@
-#include "TAD-palavra.h"
-void Interface(){
-    int escolha1,escolha2;
-    printf("Escolha uma das opções de TAD's abaixo:\n");
-    printf("1-TAD-palavra\n");printf("2-TAD-texto\n");printf("3-TAD-biblioteca");
-    scanf("%d",&escolha);
-    printf("Escolha qual das implementacoes deseja usar:\n");
-    printf("1-")
-}
--- src/main.c
+++ src/main.c
@@ -2,6 +2,5 @@
 int main(){
 
 
-//AAAAAAAAAAAA
   return 0;
 }
